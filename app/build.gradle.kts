96d864600d8a949f39c00c6fde85f8b646cc71c0
makseem-g <workspace3ds@gmail.com>
2025-03-11 07:55:39 +0300

    add new content

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.52.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
