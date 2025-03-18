384caa19281323ff669101b264b6bc7a1d59b621
makseem-g <workspace3ds@gmail.com>
2025-03-18 19:23:56 +0300

    transfer Welcome message string from App to Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,6 +5,7 @@ public class Cli {
     public static void greeting() {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name?: ");
         String name = sc.next();
         System.out.println("Hello, " + name + "!");
