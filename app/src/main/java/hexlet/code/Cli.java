016331541780a4b147738d9949036c0604f5b9e2
makseem-g <workspace3ds@gmail.com>
2025-03-18 23:50:48 +0300

    final edit Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -9,8 +9,5 @@ public class Cli {
         System.out.print("May I have your name?: ");
         String name = sc.next();
         System.out.println("Hello, " + name + "!");
-
-        sc.close();
-
     }
 }
